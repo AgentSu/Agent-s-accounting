46c5cd73245b6b8f91ac01a6060178101cbdd4dc
Agent Su <agent20001118@gmail.com>
2021-01-19 21:13:15 +0800

    'v.0.4'

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 80;
 const app = express();
+import { graphqlExpress } from 'apollo-server-express';
+const myGraphqlSchema = require('./server/schema.graphql')
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 const bodyParser = require('body-parser')
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphqlSchema}))
 
 app.get('/ping', function (req, res) {
   return res.send('pong');
