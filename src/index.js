afe687ab4b5c22b5ae7a39a872289f071b9ba041
Agent Su <agent20001118@gmail.com>
2021-01-20 11:44:45 +0800

    'v.0.4'

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const httpLink = new HttpLink({
 
 const wsLink = new WebSocketLink({
 	uri: `wss://agent-s-accounting.herokuapp.com:${process.env.PORT || 4000}/`,
-	options: { reconnect: true, timeout: 30000 }
+	options: { reconnect: true, timeout: 30000, lazy:true }
 })
 
 const link = split(
